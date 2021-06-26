3f8f15524032f7bc69c6af7761a8f11e60e82cd6
ajayvd <ajay151997@gmail.com>
2021-06-26 18:44:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-
 from __future__ import division, print_function
-# coding=utf-8
+
 import sys
 import os
 import glob
@@ -8,81 +7,88 @@ import re
 import numpy as np
 
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, \
+    decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Flask utils
+
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-#from gevent.pywsgi import WSGIServer
+
+# from gevent.pywsgi import WSGIServer
 
 # Define a flask app
+
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH ='model_resnet50.h5'
+
+MODEL_PATH = 'model_resnet50.h5'
 
 # Load your trained model
+
 model = load_model(MODEL_PATH)
-
-
 
 
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
 
     # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    ## Scaling
-    x=x/255
-    x = np.expand_dims(x, axis=0)
-   
 
-   
+    x = image.img_to_array(img)
+
+    # x = np.true_divide(x, 255)
+    # # Scaling
+
+    x = x / 255
+    x = np.expand_dims(x, axis=0)
 
     preds = model.predict(x)
-    preds=np.argmax(preds, axis=1)
-    if preds==0:
-        preds="apple"
-    elif preds==1:
-        preds="banana"
-    else if:
-        preds="mixed fruits"
-	else:
-		preds="orange"
-	    
-	
-    
-    
+    preds = np.argmax(preds, axis=1)
+    if preds == 0:
+        preds = 'apple'
+    elif preds == 1:
+        preds = 'banana'
+    elif pred == 2:
+        preds = 'mixed fruits'
+    else:
+        preds = 'orange'
+
     return preds
 
 
 @app.route('/', methods=['GET'])
 def index():
+
     # Main page
+
     return render_template('index.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+
         # Get the file from post request
+
         f = request.files['file']
 
         # Save the file to ./uploads
+
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
+        file_path = os.path.join(basepath, 'uploads',
+                                 secure_filename(f.filename))
         f.save(file_path)
 
         # Make prediction
+
         preds = model_predict(file_path, model)
-        result=preds
+        result = preds
         return result
     return None
-
 
 if __name__ == '__main__':
     app.run(debug=True)
